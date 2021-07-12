98fc2b4ec9354b9e0519e389a14f7bf7ab9dd0ec
MishaelChL <jose.mishael.chile@gmail.com>
2021-07-12 10:23:07 -0400

    envio de mails, configuracion y demas

--- TiendaOnline/settings.py
+++ TiendaOnline/settings.py
@@ -125,3 +125,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eden1998darkbreaker1998@gmail.com'
+EMAIL_HOST_PASSWORD = 'eden1998d@rkbre@ker1998'
--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,6 +1,8 @@
 from gestionPedidos.models import Articulos
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 def busqueda_productos(request):
@@ -21,5 +23,10 @@ def buscar(request):
 
 def contacto(request):
     if request.method=="POST":
+        subject = request.POST["asunto"]
+        message = request.POST["mensaje"] + " " + request.POST["email"]
+        email_from = settings.EMAIL_HOST_USER
+        recipiente_list = ["chile.jose@ficct.uagrm.edu.bo"] # a donde irá el correo
+        send_mail(subject, message, email_from, recipiente_list)
         return render(request, "gracias.html")
     return render(request, "contacto.html")
